fe3c807e897c8864a070737e5de4aa74fc99667a
ilyxakeduk <keduk2014@mail.ru>
2018-07-20 17:58:43 +0300

    switch

--- switch.cpp
+++ switch.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
+int func(int x,int z)
+{
+    return(x,z);
+}
 int main()
 {
     int x = 0;
     int z = 0;
     char y;
-    cin >> x >> y >> z ;
+    char check;
+    
+    while (check != 'N' && check != 'n')
+    {    
+        cin >> x >> y >> z ;
         switch (y){
             case '-':
                  cout << x - z << endl;
@@ -21,6 +29,16 @@ int main()
                  cout << x / z << endl;
                  break;
         }
-        system("pause");
-        return 0;
+        check = 'k';
+        // вот этот цикл
+        while (check != 'N' && check != 'n' && check != 'Y' && check != 'y')
+        { 
+            cout << "Do you want to repeat operation?: ";
+            cin >> check;
+        }
+        // Условие, почитай в книге какие бывают условия и условные операторы
+        // && тоже самое, что в паскале был AND 
+    }
+    //system("pause");
+    return 0;
 }
